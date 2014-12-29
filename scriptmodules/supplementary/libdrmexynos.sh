df47f4cd5a67b4337d5ea211c8490f1b4dc5a540
spike grobstein <me@spike.cx>
2014-12-30 04:34:45 +1030

    exynos package now uses the new style
    use the $md_build variable

--- scriptmodules/supplementary/libdrmexynos.sh
+++ scriptmodules/supplementary/libdrmexynos.sh
@@ -3,24 +3,20 @@ rp_module_desc="libdrm-exynos"
 rp_module_menus="2+"
 
 function sources_libdrmexynos() {
-  gitPullOrClone "$rootdir/supplementary/libdrm" "git://github.com/tobiasjakobi/libdrm.git"
-  pushd "$rootdir/supplementary/libdrm"
+  gitPullOrClone "$md_build" "git://github.com/tobiasjakobi/libdrm.git"
+  pushd "$md_build"
   git pull || return 1
   git checkout exynos || return 1
   popd
 }
 
 function build_libdrmexynos() {
-  pushd "$rootdir/supplementary/libdrm"
   ./autogen.sh || return 1
   ./configure --enable-exynos-experimental-api || return 1
   make || return 1
-  popd
 }
 
 function install_libdrmexynos() {
-  pushd "$rootdir/supplementary/libdrm"
   make install || return 1
-  popd
 }
 
