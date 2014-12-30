41d433a416c9467df2fbd36eee62acc20f0b0a4c
spike grobstein <me@spike.cx>
2014-12-31 03:57:18 +1030

    fixed libdrmexynos to actually build the correct version
    also follows the new structure way better

--- scriptmodules/supplementary/libdrmexynos.sh
+++ scriptmodules/supplementary/libdrmexynos.sh
@@ -4,19 +4,17 @@ rp_module_menus="2+"
 
 function sources_libdrmexynos() {
   gitPullOrClone "$md_build" "git://github.com/tobiasjakobi/libdrm.git"
-  pushd "$md_build"
-  git pull || return 1
-  git checkout exynos || return 1
-  popd
 }
 
 function build_libdrmexynos() {
-  ./autogen.sh || return 1
-  ./configure --enable-exynos-experimental-api || return 1
-  make || return 1
+  git checkout exynos
+  ./autogen.sh
+  ./configure --enable-exynos-experimental-api
+  make
+  md_ret_require="$md_build/exynos/.libs/libdrm_exynos.so"
 }
 
 function install_libdrmexynos() {
-  make install || return 1
+  make install
 }
 
